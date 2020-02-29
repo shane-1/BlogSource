ac4856ad6bd61689bd173fad6b20d053ecd7bf24
shane-1 <qq1217232433@outlook.com>
2020-02-29 19:49:21 +0800

    version 1.0

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-npm run build
+# npm run build
 
 cd docs/.vuepress/dist
+# git init
+# git config user.name "shane-1"
+# git config user.email "qq1217232433@outlook.com"
+# git add *
+# git commit -m 'version 1.0'
+# git push -f git@github.com:shane-1/shane-1.github.io.git master
+cd C:/Users/qq121/Desktop/doc/Git/Blog
 git init
 git config user.name "shane-1"
 git config user.email "qq1217232433@outlook.com"
-git add *
-git commit -m 'version 1.0'
-git push -f git@github.com:shane-1/shane-1.github.io.git master
-cd C:\Users\qq121\Desktop\doc\Git\Blog
-git init
 git add *
 git commit -m 'version 1.0'
 git push -f git@github.com:shane-1/shane-1.edit.git master
