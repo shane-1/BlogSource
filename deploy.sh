1d5b7a3068651667c4d5ead36250049eee6dd8a9
shane-1 <qq1217232433@outlook.com>
2020-10-08 16:32:09 +0800

    version 2.0

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ git init
 git config user.name "shane-1"
 git config user.email "qq1217232433@outlook.com"
 git add *
+cd C:/Users/qq121/Desktop/doc/Git/Blog
+git add googlefc62804cfc44bb36.html
 git commit -m 'version 2.0'
 git push -f git@github.com:shane-1/shane-1.github.io.git master
 cd C:/Users/qq121/Desktop/doc/Git/Blog
