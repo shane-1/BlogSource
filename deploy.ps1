af87b67f6b997df74c0002cdd54d571b3c37f1dc
shane <shane-liang@outlook.com>
2022-09-29 10:46:03 +0800

    version 4.1

--- deploy.ps1
+++ deploy.ps1
@@ -2,12 +2,14 @@
 
 cd docs/.vuepress/dist
 git init
-cp C:/Users/qq121/Desktop/doc/Git/Blog/googlefc62804cfc44bb36.html C:/Users/qq121/Desktop/doc/Git/Blog/docs/.vuepress/dist/
+cp googlefc62804cfc44bb36.html docs/.vuepress/dist/
 git add *
 git commit -m 'version 4.1'
 git push -f git@github.com:shane-1/shane-1.github.io.git master
-cd C:/Users/qq121/Desktop/doc/Git/Blog
+cd ..
+cd ..
+cd ..
 git add *
 git commit -m 'version 4.1'
-git push -f git@github.com:shane-1/edit.git master
-git push -f git@github.com:shane-1/BlogSource.git master
+git push git@github.com:shane-1/edit.git master
+git push git@github.com:shane-1/BlogSource.git master
