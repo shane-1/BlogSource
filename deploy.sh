204aaf0f4b394b4a4fc3b1ccd3af56b392701dc3
Shane Liang <39946835+shane-1@users.noreply.github.com>
2022-10-09 10:42:22 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 set -e
 
+cp googlefc62804cfc44bb36.html docs/.vuepress/dist/
 cd docs/.vuepress/dist
 git init
-cp googlefc62804cfc44bb36.html docs/.vuepress/dist/
 git add *
 git commit -m 'version 4.1'
 git push -f git@github.com:shane-1/shane-1.github.io.git main
@@ -14,4 +14,3 @@ cd ..
 git add *
 git commit -m 'version 4.1'
 git push git@github.com:shane-1/edit.git master
-git push git@github.com:shane-1/BlogSource.git master
